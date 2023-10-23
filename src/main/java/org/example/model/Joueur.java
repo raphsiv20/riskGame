ef1a06368335679690e3491ef811877409513d2c
33761 <noe.maurel31@gmail.com>
2023-10-23 17:44:18 +0200

    Classe Joueur 23/10 17:43

---
+++ src/main/java/org/example/model/Joueur.java
@@ -0,0 +1,27 @@
+package org.example.model;
+
+public class Joueur {
+    private String nomJoueur;
+    private String nomEquipe;
+    private String prenomJoueur;
+    private String dateNaissance;
+
+    public Joueur(String nomJoueur, String nomEquipe, String prenomJoueur, String dateNaissance){
+        this.nomJoueur = nomJoueur;
+        this.nomEquipe = nomEquipe;
+        this.prenomJoueur = prenomJoueur;
+        this.dateNaissance = dateNaissance;
+    }
+
+    public void debuterTour(){
+
+    }
+
+    public void placerTroupe(){
+
+    }
+
+    public void faireBataille(){
+
+    }
+}
