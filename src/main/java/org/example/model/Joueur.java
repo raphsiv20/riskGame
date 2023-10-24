4cf955656a2968c515e79e82efd05b426500198f
33761 <noe.maurel31@gmail.com>
2023-10-24 09:55:34 +0200

    Classe Soldat 24/10 9:55

--- src/main/java/org/example/model/Joueur.java
+++ src/main/java/org/example/model/Joueur.java
@@ -13,11 +13,28 @@ public class Joueur {
         this.dateNaissance = dateNaissance;
     }
 
+    public String getNomJoueur(){
+        return this.nomJoueur;
+    }
+
+    public String getNomEquipe(){
+        return this.nomEquipe;
+    }
+
+    public String getPrenomJoueur(){
+        return this.prenomJoueur;
+    }
+
+    public String getDateNaissance(){
+        return this.dateNaissance;
+    }
+
+
     public void debuterTour(){
 
     }
 
-    public void placerTroupe(){
+    public void placerTroupe(Soldat soldat, Territoire territoire){
 
     }
 
