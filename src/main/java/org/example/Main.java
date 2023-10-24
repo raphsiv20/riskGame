286832f6691df2bdeb80e571de62c83a43c7973e
ToonWrld <112943378+ToonWrld@users.noreply.github.com>
2023-10-24 11:34:57 +0200

    Y-O 24/10/23 "Ajout classes et changement samuel "

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,12 +3,12 @@ package org.example;
 import org.example.controller.AbstractControler;
 import org.example.controller.PlateauControler;
 import org.example.model.AbstractModel;
-import org.example.model.CarteJeu;
+import org.example.model.Plateau;
 import org.example.vue.RiskView;
 
 public class Main {
     public static void main(String[] args) {
-        AbstractModel model = new CarteJeu();
+        AbstractModel model = new Plateau();
         AbstractControler controler = new PlateauControler(model);
         RiskView v = new RiskView(model,controler);
         while(!model.partieTerminer()){
