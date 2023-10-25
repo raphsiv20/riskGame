bec39a1ca097630308ea82267d7decfcded1df99
ToonWrld <112943378+ToonWrld@users.noreply.github.com>
2023-10-25 11:34:41 +0200

    Y-O 25/10/23 "Gestion Manche Tour et Phases"

---
+++ src/main/java/org/example/model/Phase.java
@@ -0,0 +1,17 @@
+package org.example.model;
+
+public class Phase {
+    private String phaseTour;
+
+    public Phase(String phaseTour) {
+        this.phaseTour = phaseTour;
+    }
+
+    public String getPhase() {
+        return phaseTour;
+    }
+
+    public void setPhase(String phaseTour) {
+        this.phaseTour = phaseTour;
+    }
+}
