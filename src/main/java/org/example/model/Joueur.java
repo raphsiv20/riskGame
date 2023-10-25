4eca317921e125b72f90e64b622a68d71000e435
alexa <alexandre.lasaygues@gmail.com>
2023-10-25 09:14:45 +0200

    AL 24/10/2023 14:20
    Gestion bdd
    Ajout classe Gestion_bdd
    modif  clase joueur

--- src/main/java/org/example/model/Joueur.java
+++ src/main/java/org/example/model/Joueur.java
@@ -1,16 +1,22 @@
 package org.example.model;
+import java.util.ArrayList;
+import java.sql.*;
+import static org.example.controller.Gestion_BDD.insertNombreTerritoire;
 
 public class Joueur {
     private String nomJoueur;
     private String nomEquipe;
     private String prenomJoueur;
     private String dateNaissance;
+    private int idJoueur;
+    private ArrayList<Territoire> TerritoiresConquis = new ArrayList<Territoire>();
 
-    public Joueur(String nomJoueur, String nomEquipe, String prenomJoueur, String dateNaissance){
+    public Joueur(String nomJoueur, String nomEquipe, String prenomJoueur, String dateNaissance, int idJoueur){
         this.nomJoueur = nomJoueur;
         this.nomEquipe = nomEquipe;
         this.prenomJoueur = prenomJoueur;
         this.dateNaissance = dateNaissance;
+        this.idJoueur = idJoueur;
     }
 
     public String getNomJoueur(){
@@ -29,16 +35,19 @@ public class Joueur {
         return this.dateNaissance;
     }
 
-
-    public void debuterTour(){
-
+    public void gagnerTerritoire(Territoire territoire){
+        this.TerritoiresConquis.add(territoire);
+        insertNombreTerritoire(this.idJoueur);
     }
 
-    public void placerTroupe(Soldat soldat, Territoire territoire){
-
+    public void perdreTerritoire(Territoire territoire){
+        this.TerritoiresConquis.remove(territoire);
     }
 
-    public void faireBataille(){
+    /*public static void main(String[] args) {
+        gagnerTerritoire();
+    }*/
 
-    }
+
+
 }
