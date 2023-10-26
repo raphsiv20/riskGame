3cd3a9f418326a119701c15b8a26bc8ebb3112d8
ToonWrld <112943378+ToonWrld@users.noreply.github.com>
2023-10-26 18:13:55 +0200

    Y-O 26/10/23 "Gestion view elements"

--- src/main/java/org/example/model/Manche.java
+++ src/main/java/org/example/model/Manche.java
@@ -5,9 +5,30 @@ import java.util.HashMap;
 
 public class Manche {
     private int numManche;
+    private HashMap<Manche,Tour> listeTour;
     public Manche(int numManche) {
         this.numManche = numManche;
+        this.listeTour = new HashMap<Manche,Tour>();
     }
+
+    public void addMancheTour(Manche manche , Tour tour){
+        listeTour.put(manche,tour);
+    }
+
+    public void changementManche (){
+        if (listeTour.containsValue(4) == true){
+            for (Manche entry: listeTour.keySet()
+                 ) {
+                entry.setterNumManche(entry.getnumManche() + 1);
+                listeTour.get(entry).setNumTour(1);
+            }
+        }
+    }
+
+    public void afficherManche(){
+
+    }
+
     public void setterNumManche(int numManche) {
         this.numManche = numManche;
     }
