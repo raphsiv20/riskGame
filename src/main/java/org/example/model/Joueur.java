afc9b912a296e5ca54454c91c5e318e201046437
33761 <noe.maurel31@gmail.com>
2023-10-26 18:14:04 +0200

    fonctions carteTerritoire + map agrandie 26/10

--- src/main/java/org/example/model/Joueur.java
+++ src/main/java/org/example/model/Joueur.java
@@ -3,6 +3,7 @@ import java.util.ArrayList;
 
 import static org.example.controller.Gestion_BDD.insertNombreTerritoire;
 
+import java.util.HashMap;
 import java.util.List;
 
 public class Joueur {
@@ -164,7 +165,62 @@ public class Joueur {
         int nbFantassins = 0;
         int nbCavaliers = 0;
         int nbCanons = 0;
-        return false;
+        for (CarteTerritoire carteTerritoire: listeCarteTerritoire) {
+            if (carteTerritoire.getTypeCarte().equals("fantassin")) {
+                nbFantassins++;
+            } else if (carteTerritoire.getTypeCarte().equals("cavalier")) {
+                nbCavaliers++;
+            } else {
+                nbCanons++;
+            }
+        }
+        return (nbCanons >= 1 && nbCavaliers >= 1 && nbFantassins >= 1) || (nbCanons >= 3 || nbCavaliers >= 3 || nbFantassins >= 3);
+    }
+
+    public List<CarteTerritoire> echangerCarteTerritoires() {
+        List<CarteTerritoire> cartesEchangees = new ArrayList<>();
+        HashMap<String, List<CarteTerritoire>> cartesTerritoireClasses = new HashMap<String, List<CarteTerritoire>>();
+        cartesTerritoireClasses.put("fantassin", new ArrayList<CarteTerritoire>());
+        cartesTerritoireClasses.put("canon", new ArrayList<CarteTerritoire>());
+        cartesTerritoireClasses.put("cavalier", new ArrayList<CarteTerritoire>());
+        for (CarteTerritoire carteTerritoire: listeCarteTerritoire) {
+            if (carteTerritoire.getTypeCarte().equals("fantassin")) {
+                cartesTerritoireClasses.get("fantassin").add(carteTerritoire);
+            } else if (carteTerritoire.getTypeCarte().equals("cavalier")) {
+                cartesTerritoireClasses.get("cavalier").add(carteTerritoire);
+            } else {
+                cartesTerritoireClasses.get("canon").add(carteTerritoire);
+            }
+        }
+        if (!cartesTerritoireClasses.get("fantassin").isEmpty() && !cartesTerritoireClasses.get("cavalier").isEmpty() && !cartesTerritoireClasses.get("canon").isEmpty()) {
+            this.addSoldatsAdeployer(10);
+            cartesEchangees.add(cartesTerritoireClasses.get("fantassin").get(0));
+            cartesEchangees.add(cartesTerritoireClasses.get("cavalier").get(0));
+            cartesEchangees.add(cartesTerritoireClasses.get("canon").get(0));
+            cartesEchangees.forEach(this::removeCarteTerritoire);
+            return cartesEchangees;
+        } else if (cartesTerritoireClasses.get("canon").size() >= 3) {
+            this.addSoldatsAdeployer(8);
+            cartesEchangees.add(cartesTerritoireClasses.get("canon").get(0));
+            cartesEchangees.add(cartesTerritoireClasses.get("canon").get(1));
+            cartesEchangees.add(cartesTerritoireClasses.get("canon").get(2));
+            cartesEchangees.forEach(this::removeCarteTerritoire);
+            return cartesEchangees;
+        } else if (cartesTerritoireClasses.get("cavalier").size() >= 3) {
+            this.addSoldatsAdeployer(5);
+            cartesEchangees.add(cartesTerritoireClasses.get("cavalier").get(0));
+            cartesEchangees.add(cartesTerritoireClasses.get("cavalier").get(1));
+            cartesEchangees.add(cartesTerritoireClasses.get("cavalier").get(2));
+            cartesEchangees.forEach(this::removeCarteTerritoire);
+            return cartesEchangees;
+        } else {
+            this.addSoldatsAdeployer(3);
+            cartesEchangees.add(cartesTerritoireClasses.get("fantassin").get(0));
+            cartesEchangees.add(cartesTerritoireClasses.get("fantassin").get(1));
+            cartesEchangees.add(cartesTerritoireClasses.get("fantassin").get(2));
+            cartesEchangees.forEach(this::removeCarteTerritoire);
+            return cartesEchangees;
+        }
     }
 
 }
