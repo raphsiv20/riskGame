395f177c98b7c000aea9ac6f5a9a8f1314a48668
si <raphsiv20@gmail.com>
2023-11-07 16:08:13 +0100

    07/11/2023 RS equals territory

--- src/main/java/org/example/controller/PlateauControler.java
+++ src/main/java/org/example/controller/PlateauControler.java
@@ -401,7 +401,6 @@ public class PlateauControler extends AbstractControler {
         }
     }
 
-
     public ResultatBataille faireBataille(int attackerArmies, int attackerDice, int defenderArmies, int defenderDice) {
         try {
             Des desAtta = new Des(attackerDice);
