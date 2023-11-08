99f0a6b1b5534e59d19e70da32728569cb769081
ZhixianZHANG1 <734551538@qq.com>
2023-11-08 15:38:45 +0100

    add systeme pour creer une compet

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,11 @@
 package org.example;
 
 import org.example.controller.AbstractControler;
+import org.example.controller.InitController;
 import org.example.controller.PlateauControler;
 import org.example.model.AbstractModel;
 import org.example.model.Plateau;
+import org.example.vue.InitView;
 import org.example.vue.RiskView;
 
 import java.io.File;
@@ -12,8 +14,8 @@ import java.io.File;
 public class Main {
     public static void main(String[] args) {
         AbstractModel model = new Plateau(10, 7);
-        AbstractControler controler = new PlateauControler(model);
-        RiskView v = new RiskView(model,controler);
-        model.addObservateur(v);
+        InitView initView = new InitView();
+        InitController initController = new InitController(model,initView);
+
     }
 }
